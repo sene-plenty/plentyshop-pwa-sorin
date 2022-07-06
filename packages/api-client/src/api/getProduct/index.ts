bdb7e164ad378a15c1d93f383e48ddb6c3f6ce1e
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2022-07-06 13:53:28 +0200

    fix: refactor get product

--- packages/api-client/src/api/getProduct/index.ts
+++ packages/api-client/src/api/getProduct/index.ts
@@ -2,15 +2,6 @@ import { ProductsSearchParams } from '@vue-storefront/core';
 import { Product, Context } from 'src/types';
 
 export async function getProduct(context: Context, params: ProductsSearchParams): Promise<Product[]> {
-  // const url = new URL('/rest/io/variations', context.config.api.url);
-
-  // TODO: for test purposes we hardcode the id 1007
-  // params.id && url.searchParams.set('variationIds[]', params.id);
-  // TODO: remove this condition
-  /* params && url.searchParams.set('variationIds[]', '1007');
-
-  const { data } = await context.client.get(url.href);
-  return data.data.documents.map(document => document.data); */
 
   let url: URL;
   if (params.id) {
@@ -31,7 +22,9 @@ export async function getProduct(context: Context, params: ProductsSearchParams)
       url.searchParams.set('sorting', params.sort);
     }
   }
+
   const { data } = await context.client.get(url.href);
+
   if (params.id) {
     return data.data.documents.map(document => document.data);
   } else {
