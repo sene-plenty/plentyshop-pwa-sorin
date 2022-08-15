b0ccb1614e9353037c1993f1786d583449232a30
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2022-08-15 15:48:11 +0200

    fix: delete call

--- packages/api-client/src/api/getWishlist/index.ts
+++ packages/api-client/src/api/getWishlist/index.ts
@@ -21,7 +21,7 @@ export async function addWishlistItem(context: Context, productId: number): Prom
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export async function removeWishlistItem(context: Context, productId: number): Promise<boolean> {
   const url: URL = new URL(`/rest/io/itemWishList/${productId}`, context.config.api.url);
-  const { data } = await context.client.get(url.href);
+  const { data } = await context.client.delete(url.href);
 
   return Boolean(data.data);
 }
