8184b8fc72a1554259a287cf11fc45b5e00f3f65
ksted <kevin.stederoth@plentymarkets.com>
2022-12-07 17:16:02 +0100

    feat: add default ui component

--- packages/theme/pages/Checkout/Login.vue
+++ packages/theme/pages/Checkout/Login.vue
@@ -1,45 +1,41 @@
 <template>
   <div>
-    <SfHeading
-      v-e2e="'login-heading'"
-      :level="3"
-      :title="$t('Login')"
-      class="sf-heading--left sf-heading--no-underline title"
+    <SfPersonalDetails
+      :value="{}"
+      buttonText="Log into your account"
+      logInInfo="or fill the details below:"
+      headingTitle="Personal details"
+      :headingTitleLevel="2"
+      :inputsLabels='["First name","Last name","Your email"]'
+      additionalDetails="Enjoy these perks with your free account!"
+      :characteristics='[{"description":"Faster checkout","icon":"clock","size":"24px"},{"description":"Earn credits with every purchase","icon":"credits","size":"24px"},{"description":"Full rewards program benefits","icon":"rewards","size":"24px"},{"description":"Manage your wishlist","icon":"heart","size":"24px"}]'
+      transition="sf-fade"
+      createAccountCheckboxLabel="I want to create an account"
+      createAccountInputLabel="Create Password"
+      @input="logInput($event)"
+      @log-in="toggleLoginModal()"
+      @create-account=""
     />
-    <p class="customer__text">
-      {{ $t('Login register Text') }}
-    </p>
     <SfButton
-            :disabled="isAuthenticated"
-            type="button"
-            class="sf-button"
-            @click="toggleLoginModal()"
-          >
-            {{ $t('Login register') }}
+        type="button"
+        class="sf-button color-secondary summary__back-button"
+        @click="router.push('/checkout/billing')"
+      >
+      {{ $t('Go to shipping') }}
     </SfButton>
-    <hr class="sf-divider customer-text">
-    <SfButton
-            :disabled="isAuthenticated"
-            type="button"
-            class="sf-button"
-            @click="router.push('/checkout/billing')"
-            data-e2e="checkoutlogin-continue-as-guest"
-          >
-            {{ $t('Continue as guest') }}
-    </SfButton>
-  </div>
+    </div>
 </template>
 <script>
 import { useRouter, watch } from '@nuxtjs/composition-api';
-import { SfHeading, SfButton } from '@storefront-ui/vue';
+import { SfButton, SfPersonalDetails } from '@storefront-ui/vue';
 import { useUiState } from '~/composables';
 import { useUser } from '@vue-storefront/plentymarkets';
 
 export default {
   name: 'Login',
   components: {
-    SfHeading,
-    SfButton
+    SfButton,
+    SfPersonalDetails
   },
   setup() {
 
@@ -53,11 +49,16 @@ export default {
       }
     });
 
+    const logInput = (event) => {
+      console.log(event);
+    };
+
     return {
       router,
       isAuthenticated,
       isLoginModalOpen,
-      toggleLoginModal
+      toggleLoginModal,
+      logInput
     };
   }
 };
