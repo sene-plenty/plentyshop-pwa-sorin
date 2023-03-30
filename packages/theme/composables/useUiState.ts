c8ff4f77f55a2d2095947b2fb2c267ab16045d58
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2023-03-30 07:43:53 +0200

    fix: lint errors in uiStateHelpers (#141)
    * fix: lint errors in uiStateHelpers
    
    * fix: interface definitions
    
    ---------
    
    Co-authored-by: ksted <kevin.stederoth@plentymarkets.com>

--- packages/theme/composables/useUiState.ts
+++ packages/theme/composables/useUiState.ts
@@ -1,4 +1,4 @@
-import { reactive, computed } from '@nuxtjs/composition-api';
+import { reactive, computed, ComputedRef } from '@nuxtjs/composition-api';
 
 const state = reactive({
   isCartSidebarOpen: false,
@@ -11,50 +11,70 @@ const state = reactive({
   isLangModalOpen: false
 });
 
-const useUiState = () => {
+export interface UseUiStateResponse {
+    isCartSidebarOpen: ComputedRef<boolean>,
+    isWishlistSidebarOpen: ComputedRef<boolean>,
+    isLoginModalOpen: ComputedRef<boolean>,
+    isNewsletterModalOpen: ComputedRef<boolean>,
+    isCategoryGridView: ComputedRef<boolean>,
+    isFilterSidebarOpen: ComputedRef<boolean>,
+    isMobileMenuOpen: ComputedRef<boolean>,
+    isLangModalOpen: ComputedRef<boolean>,
+    toggleCartSidebar: () => void,
+    toggleWishlistSidebar: () => void,
+    toggleLoginModal: () => void,
+    toggleNewsletterModal: () => void,
+    changeToCategoryGridView: () => void,
+    changeToCategoryListView: () => void,
+    toggleFilterSidebar: () => void,
+    toggleMobileMenu: () => void,
+    toggleLangModal: () => void
+}
+
+const useUiState = (): UseUiStateResponse => {
   const isMobileMenuOpen = computed(() => state.isMobileMenuOpen);
-  const toggleMobileMenu = () => {
+  const toggleMobileMenu = (): void => {
     state.isMobileMenuOpen = !state.isMobileMenuOpen;
   };
 
   const isCartSidebarOpen = computed(() => state.isCartSidebarOpen);
-  const toggleCartSidebar = () => {
+  const toggleCartSidebar = (): void => {
     if (state.isMobileMenuOpen) toggleMobileMenu();
     state.isCartSidebarOpen = !state.isCartSidebarOpen;
   };
 
   const isWishlistSidebarOpen = computed(() => state.isWishlistSidebarOpen);
-  const toggleWishlistSidebar = () => {
+  const toggleWishlistSidebar = (): void => {
     if (state.isMobileMenuOpen) toggleMobileMenu();
     state.isWishlistSidebarOpen = !state.isWishlistSidebarOpen;
   };
 
   const isLoginModalOpen = computed(() => state.isLoginModalOpen);
-  const toggleLoginModal = () => {
+  const toggleLoginModal = (): void => {
     if (state.isMobileMenuOpen) toggleMobileMenu();
     state.isLoginModalOpen = !state.isLoginModalOpen;
   };
 
   const isNewsletterModalOpen = computed(() => state.isNewsletterModalOpen);
-  const toggleNewsletterModal = () => {
+  const toggleNewsletterModal = (): void => {
     state.isNewsletterModalOpen = !state.isNewsletterModalOpen;
   };
 
   const isCategoryGridView = computed(() => state.isCategoryGridView);
-  const changeToCategoryGridView = () => {
+  const changeToCategoryGridView = (): void => {
     state.isCategoryGridView = true;
   };
-  const changeToCategoryListView = () => {
+  const changeToCategoryListView = (): void => {
     state.isCategoryGridView = false;
   };
 
   const isFilterSidebarOpen = computed(() => state.isFilterSidebarOpen);
-  const toggleFilterSidebar = () => {
+  const toggleFilterSidebar = (): void => {
     state.isFilterSidebarOpen = !state.isFilterSidebarOpen;
   };
 
   const isLangModalOpen = computed(() => state.isLangModalOpen);
-  const toggleLangModal = () => {
+  const toggleLangModal = (): void => {
     state.isLangModalOpen = !state.isLangModalOpen;
   };
 
