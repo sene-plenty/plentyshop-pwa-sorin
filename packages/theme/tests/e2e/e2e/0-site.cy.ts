edd926daab46beeeaf3f2bcbd65ce16fc523d088
Kevin Stederoth <43753494+ksted@users.noreply.github.com>
2023-05-02 09:55:35 +0200

    fix: site availability test (#180)
    * fix: site availability test
    
    * docs: add explanation for test

--- packages/theme/tests/e2e/e2e/0-site.cy.ts
+++ packages/theme/tests/e2e/e2e/0-site.cy.ts
@@ -1,15 +1,22 @@
 import page from '../pages/factory';
 
+/**
+ * Compensates for https://github.com/cypress-io/cypress/issues/7062
+ * 
+ * Dummy test to check site availability on GitHub runner.
+ * Without this test, the first visit regularly fails, forcing many repeat runs on a valid PR.
+ */
+
 context('Homepage', () => {
     beforeEach(function init () {
         cy.request({
             url: '/',
-            timeout: 60000
+            timeout: 120000
         });
+        console.log("Homepage is available")
     });
-    console.log("Homepage is available")
-});
-
-it(['happyPath', 'regression'], 'Should access the homepage', function test() {
-    page.home.visit();
+    
+    it(['happyPath', 'regression'], 'Should access the homepage', function test() {
+        page.home.visit();
+    });
 });
