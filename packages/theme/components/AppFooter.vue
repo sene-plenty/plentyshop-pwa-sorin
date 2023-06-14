d135555c7aa508b7d7d0337e3e83c7feaa939234
rbadici-plenty <121930969+rbadici-plenty@users.noreply.github.com>
2023-06-14 16:06:08 +0300

    feat: removed unused columns in footer, made departments update autom… (#225)

--- packages/theme/components/AppFooter.vue
+++ packages/theme/components/AppFooter.vue
@@ -4,50 +4,16 @@
     multiple
     class="footer"
   >
-    <SfFooterColumn :title="$t('AppFooter.About us')">
-      <SfList>
-        <SfListItem
-          v-for="item in aboutUs"
-          :key="item"
-        >
-          <SfMenuItem
-            :label="$t(item)"
-          />
-        </SfListItem>
-      </SfList>
-    </SfFooterColumn>
     <SfFooterColumn :title="$t('AppFooter.Departments')">
       <SfList>
         <SfListItem
-          v-for="item in departments"
-          :key="item"
+          v-for="(category, index) in categoryTree.slice(0, 5)"
+          :key="index"
         >
           <SfMenuItem
-            :label="$t(item)"
-          />
-        </SfListItem>
-      </SfList>
-    </SfFooterColumn>
-    <SfFooterColumn :title="$t('AppFooter.Help')">
-      <SfList>
-        <SfListItem
-          v-for="item in help"
-          :key="item"
-        >
-          <SfMenuItem
-            :label="$t(item)"
-          />
-        </SfListItem>
-      </SfList>
-    </SfFooterColumn>
-    <SfFooterColumn :title="$t('AppFooter.Payment & Delivery')">
-      <SfList>
-        <SfListItem
-          v-for="item in paymentsDelivery"
-          :key="item"
-        >
-          <SfMenuItem
-            :label="$t(item)"
+            :label="categoryTreeGetters.getLabel(category)"
+            class="sf-header-navigation-item__menu-item"
+            :link="localePath(`/c/${categoryTreeGetters.getSlug(category)}`)"
           />
         </SfListItem>
       </SfList>
@@ -65,49 +31,36 @@
         </SfListItem>
       </SfList>
     </SfFooterColumn>
-    <SfFooterColumn :title="$t('AppFooter.Social')">
-      <div class="footer__socials">
-        <SfImage
-          v-for="item in social"
-          :key="item"
-          class="footer__social-image"
-          :src="addBasePath('/icons/'+item+'.svg')"
-          :alt="item"
-          width="32"
-          height="32"
-        />
-      </div>
-    </SfFooterColumn>
   </SfFooter>
 </template>
 
 <script>
-import { SfFooter, SfList, SfImage, SfMenuItem } from '@storefront-ui/vue';
+import { SfFooter, SfList, SfMenuItem } from '@storefront-ui/vue';
 import { addBasePath } from '@vue-storefront/core';
-import { useRouter } from '@nuxtjs/composition-api';
+import { computed, useRouter } from '@nuxtjs/composition-api';
+import { categoryTreeGetters, useCategory } from '@vue-storefront/plentymarkets';
 
 export default {
   components: {
     SfFooter,
     SfList,
-    SfImage,
     SfMenuItem
   },
   setup() {
     const router = useRouter();
+    const { categories, loading } = useCategory('categories');
+    const categoryTree = computed(() => loading && categories.value.map((cat) => categoryTreeGetters.getTree(cat)));
 
     return {
       router,
-      addBasePath
+      addBasePath,
+      categoryTreeGetters,
+      categoryTree
     };
   },
   data() {
     return {
-      aboutUs: ['AppFooter.Who we are', 'AppFooter.Quality in the details', 'AppFooter.Customer reviews'],
       departments: ['AppFooter.Women fashion', 'AppFooter.Men fashion', 'AppFooter.Kidswear', 'AppFooter.Home'],
-      help: ['AppFooter.Customer service', 'AppFooter.Size guide', 'AppFooter.Contact us'],
-      paymentsDelivery: ['AppFooter.Purchase terms', 'AppFooter.Guarantee'],
-      social: ['facebook', 'pinterest', 'google', 'twitter', 'youtube'],
       legal: ['AppFooter.CancellationRights', 'AppFooter.CancellationForm', 'AppFooter.LegalDisclosure',
         'AppFooter.PrivacyPolicy', 'AppFooter.TermsAndConditions', 'AppFooter.Unsubscribe'],
       legalPaths: {
@@ -125,40 +78,29 @@ export default {
 };
 </script>
 
-  <style lang="scss">
+<style lang="scss">
+.footer {
+  margin-bottom: 3.75rem;
 
-  .footer {
-    margin-bottom: 3.75rem;
+  @include for-desktop {
+    margin-bottom: 0;
+  }
+}
+
+.sf-footer {
+  @include for-desktop {
+    border-top: var(--spacer-xs) solid var(--c-primary);
+    padding-bottom: 0;
+    margin-top: var(--spacer-2xl);
+  }
+
+  &__container {
+    margin: var(--spacer-sm);
+
     @include for-desktop {
-      margin-bottom: 0;
-    }
-    &__socials {
-      display: flex;
-      justify-content: space-between;
-      margin: 0 auto var(--spacer-lg);
-      padding: var(--spacer-base) var(--spacer-xl);
-      @include for-desktop {
-        justify-content: flex-start;
-        padding: var(--spacer-xs) 0;
-        margin: 0 auto;
-      }
-    }
-    &__social-image {
-      margin: 0 var(--spacer-2xs) 0 0;
+      max-width: 69rem;
+      margin: 0 auto;
     }
   }
-  .sf-footer {
-    @include for-desktop {
-      border-top: var(--spacer-xs) solid var(--c-primary);
-      padding-bottom: 0;
-      margin-top: var(--spacer-2xl);
-    }
-    &__container {
-      margin: var(--spacer-sm);
-      @include for-desktop {
-        max-width: 69rem;
-        margin: 0 auto;
-      }
-    }
-  }
-  </style>
+}
+</style>
